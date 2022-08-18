23a0e57213e196af23c97be10a8c94f7bc9eefaa
Mikhael <turmik71@gmail.com>
2022-08-18 14:36:50 +0200

    Задача021 решена, сделаны исправления количества знаков после запятой f2

--- Task021/Program.cs
+++ Task021/Program.cs
@@ -20,4 +20,4 @@ Console.Write("Введите координаты z2: ");
 int z2 = int.Parse(Console.ReadLine());
 
 double dist = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2) + Math.Pow(z2-z1,2));
-Console.WriteLine($" {dist:f3} ");
+Console.WriteLine($" {dist:f2} ");
