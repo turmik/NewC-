7739933d8931ac04dde2251b66db1af35b88421e
Mikhael <turmik71@gmail.com>
2022-08-18 13:18:37 +0200

    Начал кодить

---
+++ Task027/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 27: Напишите функцию и запустите ее, которая принимает
+ на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int NumSum(int N)
+{
+   int b = num%10;
+   int result = 0;
+   while( b>0 )
+   {
+    num = num/10;
+    result = result + b;
+   }
+   return result;
+    
+}
+Console.WriteLine(NumSum(num)); 
+
+
+/*int a = 4;
+int b = a%10; 
+Console.WriteLine($"{b}");*/
+
+
