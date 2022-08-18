9c9a5b28a531c4fd443eba81b97b67de54ff5d40
Mikhael <turmik71@gmail.com>
2022-08-18 11:40:20 +0200

    Накидали код, проблема с телом цикла

---
+++ Task025/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+Реализовать функцию возведения в степень самостоятельно!*/
+
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: 0");
+int B = int.Parse(Console.ReadLine());
+
+int GetMult(int res)
+
+{ 
+    int c = 1;
+
+  for(int i = 1; i <= B; 1++)
+   {
+    
+   }
+   return c;
+}
+
+Console.Write(GetMult(A));
+
+
+
+
