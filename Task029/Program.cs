9d01838ec00db3c7165389d85e19b1b4967dfa18
Mikhael <turmik71@gmail.com>
2022-08-22 17:02:23 +0200

    Задача 29 решена

--- Task029/Program.cs
+++ Task029/Program.cs
@@ -3,3 +3,22 @@
 5, 0, 20 -> [1, 2, 5, 7, 19]
 3, 1, 35 -> [6, 1, 33]*/
 
+Console.Write("Введите a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+
+int[] arr(int N, int a, int b)
+
+ {
+    int [] res = new int [N];
+    for (int i = 0; i < N; i++)
+    {
+        res[i] = new Random().Next(a,b+1);
+    }
+    return res;
+}
+int[] arr1  = arr(N,a,b);  
+Console.WriteLine(String.Join(",", arr1));
