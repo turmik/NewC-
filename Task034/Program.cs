2b4ae41325931cbe03f9f9201124400f59274c04
Mikhael <turmik71@gmail.com>
2022-08-24 14:22:33 +0200

    Задача034 набросок

---
+++ Task034/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, которая покажет количество 
+чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+int[] GetArray(int size, int minValue, int maxValue) /*new Random().Next(length, 100, 999)*/
+{
+    int[] res = new int[size];
+    int result = 0;
+    
+    for(int i=0; i < size; i++)
+    {
+        res[i] = new Random().Next(100,1000);
+        if(res[i] % 2 == 0)
+        {
+          result++;
+        }
+    return result;
+    }
+}
+int[] myArray = GetArray(10, 100, 1000);          
+Console.WriteLine($"{GetArray(myArray)}");
+Console.WriteLine($"{result}");
